e3e1c52c3ee9d75d8b571354befcdc7628a3eb3b
Danli Denny Deng <danlideng@yahoo.com>
2021-10-04 12:27:25 -0700

    Lesson 1 introduction and Lesson 2 Icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { HiAcademicCap } from "react-icons/hi"
+import { MdApi } from "react-icons/md"
 
+import { IconContext } from 'react-icons'
+
+import { FaAmazon } from "react-icons/fa"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     gopinav
+     <FaAmazon color='purple' size='10rem' />
+
+      <IconContext.Provider value={{color: 'blue', size: '5rem'}}>
+        <MdApi />
+        <HiAcademicCap color='red' size='8rem' />
+      </IconContext.Provider> 
     </div>
   );
 }
